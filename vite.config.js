60664fa76e212313f93d6a0c9c6a9be85082d326
Miaya South <miayasouth2019@gmail.com>
2026-08-27 01:52:43 -0700

    configuring for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/web103prework/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
